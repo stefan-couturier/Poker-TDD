d9d61e6ef47be8165db78978efabcfe805898478
stefan-couturier <stefan.couturier@carleton.ca>
2015-09-23 19:08:30 -0400

    fixed test

--- src/poker/Testing.java
+++ src/poker/Testing.java
@@ -65,7 +65,8 @@ public class Testing {
 		assertNotEquals(hand2[3].print(), hand1[3].print());
 		assertNotEquals(hand2[4].print(), hand1[4].print());
 		
-		String x1 = (d2.cards[0] + "," + d2.cards[1] + "," + d2.cards[2] + "," + d2.cards[3] + "," + d2.cards[4]);
+		String x1 = (d2.cards[0].toString() + "," + d2.cards[1].toString() + "," + d2.cards[2].toString() + 
+				"," + d2.cards[3].toString() + "," + d2.cards[4].toString());
 		String x2 = d2.print();
 		System.out.print();
 		
