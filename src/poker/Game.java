d1d8ef959e3c9fdb1660d596756286484895a58c
stefan-couturier <stefan.couturier@carleton.ca>
2015-09-25 09:32:19 -0400

    restricted between 2-4 players

--- src/poker/Game.java
+++ src/poker/Game.java
@@ -14,8 +14,13 @@ public class Game {
 		activePlayers = 0;
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("How many players for the round?   ");
+		System.out.println("How many players for the round?(2-4)   ");
 		activePlayers = in.nextInt();
+		while(!(activePlayers > 1 && activePlayers < 5)){
+			System.out.println("Must be 2-4 players.");
+			System.out.println("How many players for the round?(2-4)   ");
+			activePlayers = in.nextInt();
+		}
 		
 		for(int x = 0; x< activePlayers;x++){
 			String n = "Dumby";
